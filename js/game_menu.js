feae11223b037ba3e39f41c75579e6d486030cfb
ShagalEugene <mreugene1990@gmail.com>
2024-12-22 19:47:37 +0300

    game_menu.js

--- js/game_menu.js
+++ js/game_menu.js
@@ -2,13 +2,13 @@ const levels = document.querySelectorAll(".level");
 const account = JSON.parse(localStorage[localStorage[currentAccount]]);
 const idUnlockedLevel = account.currentLevel;
 
-// levels.forEach(level => {
-//     if (level.id != idUnlockedLevel)
-//     {
-//         level.classList.add("disable");
+levels.forEach(level => {
+    if (level.id != idUnlockedLevel)
+    {
+        level.classList.add("disable");
 
-//         if (level.id > idUnlockedLevel)
-//             level.children[0].classList.add("closed");
-//         else level.children[0].classList.add("done");
-//     }
-// });
+        if (level.id > idUnlockedLevel)
+            level.children[0].classList.add("closed");
+        else level.children[0].classList.add("done");
+    }
+});
